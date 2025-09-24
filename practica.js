88b0bccb96feab2bed89753c3ee57f8ab016a055
alumno <alumno@Instituto.local>
2025-09-24 08:16:27 -0300

    practica

---
+++ practica.js
@@ -0,0 +1,13 @@
+//Definir la funcion
+
+//entre los parentesis ponemos los parámetros, osea valores
+function saludar(nombre){/se le pueden pasar mas de un valor. ej:nombre, apellido{
+    alert()
+}
+
+//return devuelve un valor
+
+//otro ej: 
+function calculo(n1,n2){
+    return n1*n2
+}
